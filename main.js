ec90732df7d992f5f96ffa60fe2ab18d00b6c5ae
Sheelove Dol <siesel26@gmail.com>
2022-04-12 17:27:41 -0700

    Recursion practice

---
+++ main.js
@@ -0,0 +1,10 @@
+// Write two functions that find the factorial of any number. One should use recursive, the other one a for-loop
+
+function findFactorialRecursive(number) {
+    
+}
+
+
+function findFactorialIterative(number) {
+
+}
